560aba0e0524b28e804f62798b00db945875266d
Alexie Staffer <alexie.staffer@red-gate.com>
2015-10-26 14:02:31 +0000

    Setup account.

--- BankingConsole/Program.cs
+++ BankingConsole/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using BankingKata;
 
 namespace BankingConsole
 {
@@ -10,6 +8,21 @@ namespace BankingConsole
     {
         static void Main(string[] args)
         {
+            var account = CreateAccount();
+
+            Console.WriteLine("Welcome to your account.");
+        }
+
+        private static IAccount CreateAccount()
+        {
+            var baseAccount = new Account();
+            var accountWithArrangedOverdraft = new AccountWithOverdraft(baseAccount,
+                new ArrangedOverdraft(new Money(-1500m), new Money(10m)));
+            var accountWithUnarrangedOverdraft = new AccountWithOverdraft(accountWithArrangedOverdraft,
+                new UnarrangedOverdraft(new Money(-4000m)));
+
+            baseAccount.Deposit(DateTime.Now, new Money(1000m));
+            return accountWithUnarrangedOverdraft;
         }
     }
 }
