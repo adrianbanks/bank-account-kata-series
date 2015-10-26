9fc5a4a4a50496fb2553814cf81691466fc904b2
Alexie Staffer <alexie.staffer@red-gate.com>
2015-10-26 14:28:53 +0000

    R# Cleanup

--- BankingConsole/AccountBuilder.cs
+++ BankingConsole/AccountBuilder.cs
@@ -5,44 +5,44 @@ namespace BankingConsole
 {
     public class AccountBuilder
     {
-        private Money startingBalance = new Money(0);
-        private Money arrangedOverdraftLimit;
-        private Money unarrangedOverdraftLimit;
-        private Money overdraftCharge;
+        private Money _startingBalance = new Money(0);
+        private Money _arrangedOverdraftLimit;
+        private Money _unarrangedOverdraftLimit;
+        private Money _overdraftCharge;
 
         public AccountBuilder WithStartingBalance(Money startingBalance)
         {
-            this.startingBalance = startingBalance;
+            _startingBalance = startingBalance;
             return this;
         }
 
         public AccountBuilder WithOverdraftChargeOf(Money overdraftCharge)
         {
-            this.overdraftCharge = overdraftCharge;
+            _overdraftCharge = overdraftCharge;
             return this;
         }
 
         public AccountBuilder WithArrangedOverdraftLimitOf(Money overdraftLimit)
         {
-            arrangedOverdraftLimit = overdraftLimit;
+            _arrangedOverdraftLimit = overdraftLimit;
             return this;
         }
 
         public AccountBuilder WithUnarrangedOverdraftLimitOf(Money overdraftLimit)
         {
-            unarrangedOverdraftLimit = overdraftLimit;
+            _unarrangedOverdraftLimit = overdraftLimit;
             return this;
         }
 
         public IAccount Build()
         {
             var baseAccount = new Account();
-            var arrangedOverdraft = new ArrangedOverdraft(arrangedOverdraftLimit, overdraftCharge);
+            var arrangedOverdraft = new ArrangedOverdraft(_arrangedOverdraftLimit, _overdraftCharge);
             var accountWithArrangedOverdraft = new AccountWithOverdraft(baseAccount, arrangedOverdraft);
-            var unarrangedOverdraft = new UnarrangedOverdraft(unarrangedOverdraftLimit);
+            var unarrangedOverdraft = new UnarrangedOverdraft(_unarrangedOverdraftLimit);
             var accountWithUnarrangedOverdraft = new AccountWithOverdraft(accountWithArrangedOverdraft, unarrangedOverdraft);
 
-            baseAccount.Deposit(DateTime.Now, startingBalance);
+            baseAccount.Deposit(DateTime.Now, _startingBalance);
             return accountWithUnarrangedOverdraft;
         }
     }
